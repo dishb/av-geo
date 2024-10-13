b6b61dfdf287900bf386456a1fb76526bb0bf385
dishb <code.dishb@gmail.com>
2024-10-12 21:49:56 -0700

    feat: add functionality to buttons/links

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Select all elements needed in the script
 
+const content = document.querySelector('#content');
+const scrollDown = document.querySelector("#action-button");
+
+// Scroll down to content functionality
+
+scrollDown.addEventListener('click', () => {
+    content.scrollIntoView();
+})
