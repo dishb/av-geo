f0b38895ba1d256fb6645f9f094350fe1f072ae5
dishb <code.dishb@gmail.com>
2024-10-14 13:59:12 -0700

    fix: error in script when selecting element

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Select all elements needed in the script
 
 const content = document.querySelector('#content');
-const scrollDown = document.querySelector("#action-button");
+const scrollDown = document.querySelector("#action-btn");
 
 // Scroll down to content functionality
 
 scrollDown.addEventListener('click', () => {
     content.scrollIntoView();
-})
+});
