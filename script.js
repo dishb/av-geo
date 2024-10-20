4812df0c542178c0abbd5c734d8e125ebae89feb
dishb <code.dishb@gmail.com>
2024-10-19 21:33:55 -0700

    chore: refactor HTML/JS to use buttons

--- script.js
+++ script.js
@@ -2,9 +2,31 @@
 
 const content = document.querySelector('#content');
 const scrollDown = document.querySelector("#action-btn");
+const iconBtn = document.querySelector('#icon-btn');
+const navBar = document.querySelector('nav');
+const navItems = document.querySelectorAll('.nav-item');
 
 // Scroll down to content functionality
 
 scrollDown.addEventListener('click', () => {
     content.scrollIntoView();
 });
+
+// Scroll to home functionality
+iconBtn.addEventListener('click', () => {
+    nav.scrollIntoView();
+})
+
+scrollDown.addEventListener('click', () => {
+    content.scrollIntoView();
+});
+
+// Nav items scroll down functionality
+navItems.forEach(navItem => {
+    item.addEventListener('click', () => {
+        const targetId = navItem.textContent.toLowerCase();
+        const targetElement = document.getElementById(targetId);
+        targetElement.scrollIntoView();
+    });
+});
+
